b7dee79c930637c9e8ae4b66b9dfb0124635c91d
ANJU YADAV <anjuyad2810@gmail.com>
2025-07-05 16:49:35 +0530

    Initial commit

---
+++ backened/config/db.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+
+export const connectDB=async()=>{
+    await mongoose.connect('mongodb+srv://anjuyad2810:EjWsPWvkZ77NEvSW@cluster0.qeh4v5y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(()=>console.log('DB CONNECTED'));
+}
